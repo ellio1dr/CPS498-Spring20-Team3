8f45c32f391f25feb8afabd75c21a3ca1fc09c87
Thomp9j <44217915+Thomp9j@users.noreply.github.com>
2020-04-28 23:24:48 -0400

    Add files via upload
    Simple timer function with a boolean value marking if player has completed the level and to stop the timer.

---
+++ assets/timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool complete = false;
+    private float StarTime;
+    public Text TimerText;
+
+    void Start()
+    {
+        StarTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (complete)
+            return;
+
+        float f = Time.time - StarTime;
+
+        string minutes = ((int)f / 60).ToString();
+        string seconds = (f % 60).ToString("f0");
+
+        TimerText.text = minutes + ":" + seconds;
+    }
+
+    public void Finnish()
+    {
+
+        complete = true;
+        Color c = new Color(0, 1, 0, 1);
+        TimerText.color = c;
+
+
+    }
+
+}
